6053b16edbc068375e859b6410adca0175e673e0
mkn00 <92109320+mkn00@users.noreply.github.com>
2022-06-02 18:48:09 +0200

    Add files via upload

---
+++ Lab2/lab_2_zadanie/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2_zadanie
+{
+	internal class Product : IThing
+	{
+		private string _name;
+		public string Name 
+		{ 
+			get
+			{
+				return _name;
+			}
+			set
+			{
+				_name = value;
+			}
+		}
+
+		public Product(string name)
+		{
+			this._name = name;
+		}
+
+		public virtual void Print(string prefix)
+		{
+
+		}
+	}
+}
---
+++ Lab2/lab_2_zadanie/Shop.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2_zadanie
+{
+	internal class Shop : IThing
+	{
+		private string _name;
+		private Person[] _people;
+		private Product[] _products;
+		public string Name
+		{
+			get 
+			{ 
+				return _name; 
+			}
+			set 
+			{ 
+				_name = value; 
+			}
+		}
+		public Shop(string name, Person[] people, Product[] products)
+		{
+			this._name = name;
+			this._people = people;
+			this._products = products;
+		}
+		public void Print()
+		{
+			Console.WriteLine($"Shop: {Name}");
+			Console.WriteLine("-- People: --");
+			foreach (Person p in _people)
+			{
+				p.Print("\t");
+			}
+			Console.WriteLine("-- Products: --");
+			foreach (Product p in _products)
+			{
+				p.Print("\t");
+			}
+		}
+	}
+}
