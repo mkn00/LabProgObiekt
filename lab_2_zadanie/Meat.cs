cee8951661d8e77f27232539a4466328f1d77e6f
mkn00 <92109320+mkn00@users.noreply.github.com>
2022-06-02 16:16:54 +0200

    Add files via upload

---
+++ lab_2_zadanie/Meat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2_zadanie
+{
+	internal class Meat : Product
+	{
+		private double _weight;
+		public double Weight
+		{
+			get
+			{
+				return _weight;
+			}
+			set
+			{
+				_weight = value;
+			}
+		}
+
+		public Meat(string name, double weight) : base(name)
+		{
+			this._weight = weight;
+		}
+
+		public override void Print(string prefix)
+		{
+			Console.WriteLine($"{prefix}{Name} ({Weight} kg)");
+		}
+
+	}
+}
---
+++ lab_2_zadanie/Seller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2_zadanie
+{
+	internal class Seller : Person
+	{
+		public Seller(string name, int age) : base(name, age)
+		{
+			this.Name = name;
+			this.Age = age;
+		}
+		public override void Print(string prefix)
+		{
+			Console.WriteLine($"{prefix}Seller: {Name} ({Age} y.o.)");
+		}
+	}
+}
